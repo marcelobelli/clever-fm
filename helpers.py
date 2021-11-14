d51094ec78296b5070b65508130cb6aca8f4d3bf
Marcelo Belli <marcelo@belli.me>
2021-11-14 12:39:27 -0300

    Refactor timestamp_to_seconds

--- helpers.py
+++ helpers.py
@@ -1,14 +1,14 @@
 from collections import Counter
 
+
 def timestamp_to_seconds(timestamp: str) -> int:
     c = Counter(timestamp)
 
     match c[":"]:
         case 1:
-            minutes, seconds = timestamp.split(":")
-            return int(minutes) * 60 + int(seconds)
+            hours, minutes, seconds = 0, *timestamp.split(":")
 
         case 2:
             hours, minutes, seconds = timestamp.split(":")
-            return int(hours) * 3600 + int(minutes) * 60 + int(seconds)
 
+    return int(hours) * 3600 + int(minutes) * 60 + int(seconds)
