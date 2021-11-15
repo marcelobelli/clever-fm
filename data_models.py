144567340a24f192ac417458f6c966016008c4c5
Marcelo Belli <marcelo@belli.me>
2021-11-14 22:27:05 -0300

    Create words_per_second_from_excerpt method

--- data_models.py
+++ data_models.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Iterator, Optional
+from typing import Iterator
 
 from exceptions import TimestampFormatException
 from helpers import timestamp_to_seconds
@@ -10,7 +10,16 @@ class Transcript:
     pass
 
     @classmethod
-    def raw_transcript_to_excerpts(cls, raw_transcript: list[str]) -> Iterator[list[int, str, Optional[int]]]:
+    def words_per_second_from_excerpt(cls, excerpt: list) -> Iterator[tuple[int, list[str]]]:
+        time_range = excerpt[2] - excerpt[0]
+        words = excerpt[1].split(" ")
+        words_per_sec = round(len(words) / time_range)
+
+        for i in range(0, len(words), words_per_sec):
+            yield words[i: i + words_per_sec]
+
+    @classmethod
+    def raw_transcript_to_excerpts(cls, raw_transcript: list[str]) -> Iterator[list]:
         timestamp = timestamp_to_seconds(raw_transcript[0])
         excerpt = [timestamp]
 
--- tests/test_transcript_data_model.py
+++ tests/test_transcript_data_model.py
@@ -37,3 +37,50 @@ def test_transform_raw_transcript_into_clean_full_excerpts(raw_transcript):
     excerpts = [excerpt for excerpt in Transcript.raw_transcript_to_excerpts(raw_transcript)]
 
     assert excerpts == expected
+
+
+def test_get_words_per_second_from_excerpt_with_initial_and_end_timestamp():
+    excerpt = [
+        1,
+        "When I was a kid, apples were garbage. They were called Red Delicious and they were red. They were not delicious. They looked beautiful, but then you bite into it, and almost always it would be mushy and mealy, just nasty.",
+        15,
+    ]
+    expected = [
+        ["When", "I", "was"],
+        ["a", "kid,", "apples"],
+        ["were", "garbage.", "They"],
+        ["were", "called", "Red"],
+        ["Delicious", "and", "they"],
+        ["were", "red.", "They"],
+        ["were", "not", "delicious."],
+        ["They", "looked", "beautiful,"],
+        ["but", "then", "you"],
+        ["bite", "into", "it,"],
+        ["and", "almost", "always"],
+        ["it", "would", "be"],
+        ["mushy", "and", "mealy,"],
+        ["just", "nasty."],
+    ]
+    result = [r for r in Transcript.words_per_second_from_excerpt(excerpt)]
+
+    assert result == expected
+
+
+def test_get_words_per_second_from_excerpt_from_the_middle_of_the_transcript():
+    excerpt = [
+        15,
+        "It was a really bad time to be an apple eater. It was also a really bad time to be an apple grower.",
+        21,
+    ]
+    expected = [
+        ["It", "was", "a", "really"],
+        ["bad", "time", "to", "be"],
+        ["an", "apple", "eater.", "It"],
+        ["was", "also", "a", "really"],
+        ["bad", "time", "to", "be"],
+        ["an", "apple", "grower."],
+    ]
+
+    result = [r for r in Transcript.words_per_second_from_excerpt(excerpt)]
+
+    assert result == expected
